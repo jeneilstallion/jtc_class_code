af6a5fe789a05ee399cd9158c3d3e481a1cf2c7f
Jeneil Stallion <jeneilstallion@gmail.com>
2020-09-22 19:55:03 -0400

    celcius is hotter than farienheit

---
+++ challenges/03_primitive_data_types_part_1/temperature.py
@@ -0,0 +1,14 @@
+#Challenge 1
+celcius_100 = 100.0
+print(celcius_100)
+#2
+celsius_0 = (0 - 32) * 5/9
+print(celsius_0)
+
+print((34.2- 32) * 5/9)
+
+print ((5*9/5) + 32)
+
+#5
+print ((85.1 - 32) * 5/9)
+print ((30.2 - 32) * 5/9)
